7c4457c7e7f40b33ea06fc7041bfb1d1f019a9ce
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-01-03 23:43:38 +0200

    rm old

--- Window-Control/src/core/config_parser.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <unordered_map>
-#include <string>
-
-void read_config(bool &move_speed, bool &move_duration, bool &run_at_startup)
-{
-    
-}
--- Window-Control/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "core/application.hpp"
-
-// add keybinds to resize and move wiith mouse as well aywherenn
-
-int main()
-{
-    Application application;
-    application.run();
-}
