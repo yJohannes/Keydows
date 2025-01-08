7979218d8db204521b54a024590cf4e78c6bcd33
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-01-08 18:03:44 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 int APIENTRY WinMain(
     HINSTANCE h_instance,
     HINSTANCE /*hPrevInstance*/,
-    LPSTR    /*lpCmdLine*/,
-    int       n_cmd_show)
+    LPSTR     /*lpCmdLine*/,
+    int       /*n_cmd_show*/)
 {
-    Application app(h_instance, n_cmd_show);
+    Application app(h_instance);
     return app.run();
 }
