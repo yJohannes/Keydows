b7bbb3ec3d4d1d94185fb7fff1690a73b09ae297
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-01-16 20:49:31 +0200

    Move

---
+++ src/utils/hotkeys.cpp
@@ -0,0 +1,31 @@
+#include "hotkeys.h"
+
+namespace hotkey
+{
+    unsigned int registered_key_count = 0; // Define the static variable here
+
+    void register_key(HWND h_wnd, int hk_id, int mod, int vk)
+    {
+        if (!::RegisterHotKey(h_wnd, hk_id, mod, vk))
+        {
+            std::cerr << "Failed to register hotkey 0x"
+            << std::hex << std::uppercase << vk
+            << "!" << std::endl;
+            throw std::runtime_error("Hotkey registration failed");
+        }
+        registered_key_count++;
+    }
+
+    void unregister_hotkeys(HWND h_wnd)
+    {
+        for (int hk_id = 0; hk_id < registered_key_count; ++hk_id)
+        {
+            if (!::UnregisterHotKey(h_wnd, hk_id))
+            {
+                std::cerr << "Failed to unregister hotkey ID " << hk_id << "!" << std::endl;
+                throw std::runtime_error("Hotkey unregistration failed");
+            }
+        }
+        registered_key_count = 0;
+    }
+} // namespace hotkey
