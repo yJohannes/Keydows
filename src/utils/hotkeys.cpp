fcb8e700794f98750694546b363890282965f6f4
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-01-20 21:23:16 +0200

    Remove stuff

--- src/utils/hotkeys.cpp
+++ src/utils/hotkeys.cpp
@@ -2,19 +2,12 @@
 
 namespace hotkey
 {
-    unsigned int register_counter = 1;
-
-    unsigned int register_hotkey(HWND h_wnd, int hk_id, int mod, int vk)
+    void register_hotkey(HWND h_wnd, int hk_id, int mod, int vk)
     {
-        if (::RegisterHotKey(h_wnd, hk_id, mod, vk))
-        {
-            return register_counter++;
-        }
-        else
+        if (!::RegisterHotKey(h_wnd, hk_id, mod, vk))
         {
             std::cerr << "Failed to register hotkey " << hk_id << "!" << std::endl;
             throw std::runtime_error("Hotkey registration failed");
-            return UINT_MAX;
         }
     }
 
@@ -26,15 +19,4 @@ namespace hotkey
             throw std::runtime_error("Hotkey unregistration failed");
         }
     }
-
-    void unregister_all_hotkeys(HWND h_wnd)
-    {
-        for (int hk_id = 0; hk_id < register_counter; ++hk_id)
-        {
-            unregister_key(h_wnd, hk_id);
-        }
-
-        register_counter = 1;
-    }
-
 } // namespace hotkey
--- src/utils/hotkeys.h
+++ src/utils/hotkeys.h
@@ -1,14 +1,10 @@
 #pragma once
-
 #include <windows.h>
 #include <iostream>
-#include <unordered_map>
 
 namespace hotkey
 {
     // Simple unique hotkey ID 
-    extern unsigned int register_counter;
-    unsigned int register_hotkey(HWND h_wnd, int hk_id, int mod, int vk);
+    void register_hotkey(HWND h_wnd, int hk_id, int mod, int vk);
     void unregister_key(HWND h_wnd, int hk_id);
-    void unregister_all_hotkeys(HWND h_wnd);
 };
