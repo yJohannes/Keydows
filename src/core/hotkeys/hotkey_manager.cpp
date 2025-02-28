d25e871586686c4a670f6d419c3c28ab6c4a60a5
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-02-28 14:38:45 +0200

    Fix bug with hotkey registration

--- src/core/hotkeys/hotkey_manager.cpp
+++ src/core/hotkeys/hotkey_manager.cpp
@@ -2,9 +2,11 @@
 
 std::unordered_map<int, HWND> HotkeyManager::m_hotkey_map;
 
+// void f(void (*f)(void)) {(**(&f))();}
+
 int HotkeyManager::register_hotkey(HWND h_wnd, int mod, int vk)
 {
-    int id = 0;
+    int id = 1; // 0 is invalid
 
     for (const auto& kv : m_hotkey_map)
     {
@@ -16,9 +18,11 @@ int HotkeyManager::register_hotkey(HWND h_wnd, int mod, int vk)
 
     if (!::RegisterHotKey(h_wnd, id, mod, vk))
     {
-        std::cerr << "Failed to register hotkey " << id << "!" << std::endl;
+        std::cerr << "Failed to register hotkey ID " << id << " with VK " << (char)vk << "!" << std::endl;
         throw std::runtime_error("Hotkey registration failed");
     }
+
+    std::cout << "Registered hotkey ID " << id << " with VK " << (char)vk << " for hwnd " << h_wnd << ".\n";
 
     m_hotkey_map[id] = h_wnd;
     return id;
@@ -42,3 +46,19 @@ void HotkeyManager::unregister_all_hotkeys()
         unregister_hotkey(kv.second, kv.first);
     }
 }
+
+// void HotkeyManager::invoke_hotkey_callback(WPARAM triggered_hotkey_id)
+// {
+//     std::cout << "ID: " << triggered_hotkey_id << " invoked\n";
+
+//     if (auto it = m_hotkey_map.find(triggered_hotkey_id); it != m_hotkey_map.end())
+//     {
+//         auto& [_, callback] = it->second;
+//         if (callback != nullptr)
+//             callback(triggered_hotkey_id);
+//     }
+//     else
+//     {
+//         std::cerr << "Invalid hotkey ID " << triggered_hotkey_id << "!" << std::endl;
+//     }
+// }
--- src/core/hotkeys/hotkey_manager.h
+++ src/core/hotkeys/hotkey_manager.h
@@ -5,6 +5,8 @@
 #include <windows.h>
 #include <iostream>
 #include <unordered_map>
+#include <utility>
+#include <functional>
 
 class HotkeyManager
 {
@@ -13,13 +15,15 @@ public:
     /// @param h_wnd 
     /// @param mod 
     /// @param vk 
+    /// @param callback Callback function which can be nullptr
     /// @return Hotkey ID
-
     [[nodiscard]]
     static int register_hotkey(HWND h_wnd, int mod, int vk);
     static void unregister_hotkey(HWND h_wnd, int hotkey_id);
     static void unregister_all_hotkeys();
+    // static void invoke_hotkey_callback(WPARAM triggered_hotkey_id);
 private:
+
     static std::unordered_map<int, HWND> m_hotkey_map;
 };
 
