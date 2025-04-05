69f88abafe9146911ad74ad0980cb974a17829fc
Johannes Ylinen <141926330+yJohannes@users.noreply.github.com>
2025-04-05 13:31:37 +0300

    Add Hadamard product

--- src/utils/vec.h
+++ src/utils/vec.h
@@ -23,8 +23,10 @@ struct Vec2
     constexpr Vec2 operator-(const Vec2& v) const { return Vec2(x - v.x, y - v.y); }
     Vec2& operator-=(const Vec2& v) { x -= v.x; y -= v.y; return *this; }
 
+    constexpr Vec2 operator*(const Vec2& v) const { return Vec2(x * v.x, y * v.y); }
     constexpr Vec2 operator*(T scalar) const { return Vec2(x * scalar, y * scalar); }
     Vec2& operator*=(T scalar) { x *= scalar; y *= scalar; return *this; }
+
 
     constexpr Vec2 operator/(T scalar) const { return Vec2(x / scalar, y / scalar); }
     Vec2& operator/=(T scalar) { x /= scalar; y /= scalar; return *this; }
